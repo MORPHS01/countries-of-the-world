c7743b063fefe371f227c938d98b3225e98c2517
MORPHS01 <ayonimofe2202@gmail.com>
2026-07-31 15:21:31 +0100

    feature/ small fixes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,16 +1,22 @@
-
-
 function Footer(props) {
-    const theme = props.theme
-    return(
-        <div id={theme}>
-            <footer>
-                <div className="container">
-                    <p>Developed by Ayonimofe Atoyebi</p>
-                </div>
-            </footer>
+  const theme = props.theme;
+  return (
+    <div id={theme}>
+      <footer>
+        <div className="container">
+          <p>
+            Developed by{" "}
+            <a
+              href="https://ayonimofe-portfolio-website.vercel.app/"
+              style={{ color: "cyan" }}
+            >
+              Ayonimofe Atoyebi
+            </a>
+          </p>
         </div>
-    );
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
